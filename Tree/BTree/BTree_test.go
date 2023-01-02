3e9247fe5dd262ad9dbd720b01919182888ae12d
Oyal2 <oyalfokshner@gmail.com>
2023-01-02 12:45:27 -0500

    Update BTree_test.go

--- Tree/BTree/BTree_test.go
+++ Tree/BTree/BTree_test.go
@@ -4,10 +4,36 @@ import (
 	"testing"
 )
 
-func TestInsert1(t *testing.T) {
-	list := New[int](3)
-	for i := 1; i < 7; i++ {
-		list.Insert(i)
+func TestBTree(t *testing.T) {
+	// Create a new B-Tree with a maximum of 3 children per node
+	tree := New[int](3)
+
+	// Insert some elements into the tree
+	tree.Insert(1)
+	tree.Insert(2)
+	tree.Insert(3)
+	tree.Insert(4)
+	tree.Insert(5)
+
+	// Check that the number of elements stored in the tree is correct
+	if tree._numberStored != 5 {
+		t.Errorf("Expected number of elements to be 5, got %d", tree._numberStored)
 	}
-	return
+
+	// Check the structure of the tree
+	if tree._root.N != 2 {
+		t.Errorf("Expected root node to have 2 keys, got %d", tree._root.N)
+	}
+	if tree._root.Leaf != false {
+		t.Errorf("Expected root node to be an internal node, got leaf node")
+	}
+	if tree._root.Children[0].N != 1 {
+		t.Errorf("Expected root's first child to have 1 key, got %d", tree._root.Children[0].N)
+	}
+	if tree._root.Children[1].N != 1 {
+		t.Errorf("Expected root's second child to have 1 key, got %d", tree._root.Children[1].N)
+	}
+	if tree._root.Children[2].N != 1 {
+		t.Errorf("Expected root's third child to have 1 key, got %d", tree._root.Children[2].N)
+	}
 }
